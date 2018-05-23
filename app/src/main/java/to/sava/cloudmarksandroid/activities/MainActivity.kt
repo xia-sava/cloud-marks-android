1957a811d73e0caf5dbb28e114dfd308cdca7362
ISIGE, Akira <xia@silvia.com>
2018-05-23 18:09:31 +0900

    SettingsActivity 追加

--- app/src/main/java/to/sava/cloudmarksandroid/activities/MainActivity.kt
+++ app/src/main/java/to/sava/cloudmarksandroid/activities/MainActivity.kt
@@ -2,6 +2,10 @@ package to.sava.cloudmarksandroid.activities
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import org.jetbrains.anko.startActivity
+import org.jetbrains.anko.toast
 import to.sava.cloudmarksandroid.R
 
 class MainActivity : AppCompatActivity() {
@@ -9,5 +13,31 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.main_menu, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
+        return when (item?.itemId) {
+            R.id.main_menu_settings -> {
+                startActivity<SettingsActivity>()
+                true
+            }
+            R.id.main_menu_load -> {
+                toast("読むぜ！")
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
+
+    override fun onPrepareOptionsMenu(menu: Menu?): Boolean {
+        menu?.findItem(R.id.main_menu_load)?.setEnabled(false)
+        return super.onPrepareOptionsMenu(menu)
     }
 }
