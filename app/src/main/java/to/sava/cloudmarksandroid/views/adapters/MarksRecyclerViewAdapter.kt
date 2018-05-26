6d71a6e7f03d14805f7612a02b0c08fc4e99ef73
ISIGE, Akira <xia@silvia.com>
2018-05-26 19:39:42 +0900

    アイコン等を入れてレイアウトを整えた

--- app/src/main/java/to/sava/cloudmarksandroid/views/adapters/MarksRecyclerViewAdapter.kt
+++ app/src/main/java/to/sava/cloudmarksandroid/views/adapters/MarksRecyclerViewAdapter.kt
@@ -1,10 +1,10 @@
 package to.sava.cloudmarksandroid.views.adapters
 
 import android.support.v7.widget.RecyclerView
-import android.support.v7.widget.RecyclerView.NO_ID
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import android.widget.TextView
 import io.realm.RealmRecyclerViewAdapter
 import io.realm.RealmResults
@@ -12,6 +12,7 @@ import to.sava.cloudmarksandroid.fragments.MarksFragment.OnListItemClickedListen
 import kotlinx.android.synthetic.main.fragment_marks.view.*
 import to.sava.cloudmarksandroid.R
 import to.sava.cloudmarksandroid.models.MarkNode
+import to.sava.cloudmarksandroid.models.MarkType
 import java.util.*
 
 
@@ -25,10 +26,10 @@ class MarksRecyclerViewAdapter(
         private val itemClickedListener: OnListItemClickedListener?)
     : RealmRecyclerViewAdapter<MarkNode, MarksRecyclerViewAdapter.MarksViewHolder>(value, true) {
 
-    private val mOnClickListener: View.OnClickListener
+    private val onClickListener: View.OnClickListener
 
     init {
-        mOnClickListener = View.OnClickListener { v ->
+        onClickListener = View.OnClickListener { v ->
             val item = v.tag as MarkNode
             // Notify the active callbacks interface (the activity, if the fragment is attached to
             // one) that an item has been selected.
@@ -38,8 +39,7 @@ class MarksRecyclerViewAdapter(
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MarksViewHolder {
-        val view = LayoutInflater.from(parent.context)
-                .inflate(R.layout.fragment_marks, parent, false)
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.fragment_marks, parent, false)
         val holder = MarksViewHolder(view)
         holder.marksView.setOnClickListener {
             val mark = getItem(holder.adapterPosition)
@@ -53,8 +53,13 @@ class MarksRecyclerViewAdapter(
     override fun onBindViewHolder(holder: MarksViewHolder, position: Int) {
         getItem(position)?.let {
             holder.data = it
-            holder.idView.text = it.id.substring(0..2)
             holder.contentView.text = it.title
+            val (icon, chevron) = when (it.type) {
+                MarkType.Folder -> Pair(R.drawable.ic_folder_open_black_24dp, View.VISIBLE)
+                MarkType.Bookmark -> Pair(R.drawable.ic_bookmark_border_black_24dp, View.INVISIBLE)
+            }
+            holder.contentView.setCompoundDrawablesWithIntrinsicBounds(icon, 0, 0, 0)
+            holder.rightChevron.visibility = chevron
         }
     }
 
@@ -62,14 +67,14 @@ class MarksRecyclerViewAdapter(
         val item = getItem(index)
         return when {
             item !== null -> UUID.fromString(item.id).leastSignificantBits
-            else -> NO_ID
+            else -> RecyclerView.NO_ID
         }
     }
 
     inner class MarksViewHolder(val view: View) : RecyclerView.ViewHolder(view) {
         val marksView: View = view.marks
-        val idView: TextView = view.item_number
         val contentView: TextView = view.content
+        val rightChevron: ImageView = view.rightChevron
         lateinit var data: MarkNode
     }
 }
