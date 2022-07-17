f45210a5144389d1826836fbf1cbd1ab38932d73
ISIGE, Akira <xia@silvia.com>
2022-07-17 14:02:53 +0900

    Jetpack Compose initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,18 @@
+
+buildscript {
+    extra.apply {
+        set("kotlinVersion", "1.6.10")
+        set("composeVersion", "1.1.1")
+    }
+}
+
+// Top-level build file where you can add configuration options common to all sub-projects/modules.
+plugins {
+    id("com.android.application") version "7.2.1" apply false
+    id("com.android.library") version "7.2.1" apply false
+    id("org.jetbrains.kotlin.android") version "1.6.10" apply false
+}
+
+tasks.register<Delete>("clean").configure {
+    delete(rootProject.buildDir)
+}
