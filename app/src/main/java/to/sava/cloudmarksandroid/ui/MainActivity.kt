64a87664ffeacbe520f575625daf90359321b094
ISIGE, Akira <xia@silvia.com>
2022-08-14 22:21:25 +0900

    MainPage は MainActivity に書いちゃうので良くない？

--- app/src/main/java/to/sava/cloudmarksandroid/ui/MainActivity.kt
+++ app/src/main/java/to/sava/cloudmarksandroid/ui/MainActivity.kt
@@ -3,18 +3,24 @@ package to.sava.cloudmarksandroid.ui
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.foundation.Image
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material.icons.filled.Menu
+import androidx.compose.runtime.*
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.currentBackStackEntryAsState
+import androidx.navigation.compose.rememberNavController
 import dagger.hilt.android.AndroidEntryPoint
-import to.sava.cloudmarksandroid.databases.repositories.MarkNodeRepository
+import to.sava.cloudmarksandroid.R
 import to.sava.cloudmarksandroid.ui.theme.CloudMarksAndroidTheme
-import javax.inject.Inject
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
-
-    @Inject
-    lateinit var repos: MarkNodeRepository
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -26,11 +32,91 @@ class MainActivity : ComponentActivity() {
     }
 }
 
-
-@Preview(showBackground = true)
 @Composable
-fun MainPagePreview() {
-    CloudMarksAndroidTheme {
-        MainPage()
+fun MainPage(modifier: Modifier = Modifier) {
+    val navController = rememberNavController()
+    val scaffoldState = rememberScaffoldState()
+    val navBackStack by navController.currentBackStackEntryAsState()
+    Scaffold(
+        topBar = {
+            CloudMarksTopAppBar(
+                showBackButton = navBackStack?.destination?.route != "marks",
+                disableSettingsMenu = navBackStack?.destination?.route == "settings",
+                onClickSettings = { navController.navigate("settings") },
+                disableLoadMenu = false,
+                onClickLoad = { /* Load() */ },
+                onClickBack = { navController.popBackStack() },
+            )
+        },
+        modifier = modifier,
+        scaffoldState = scaffoldState,
+    ) {
+        NavHost(navController, startDestination = "marks") {
+            composable("marks") {
+                MarksScreen()
+            }
+            composable("settings") {
+                Settings()
+            }
+        }
     }
 }
+
+@Composable
+private fun CloudMarksTopAppBar(
+    showBackButton: Boolean = false,
+    disableSettingsMenu: Boolean = false,
+    onClickSettings: () -> Unit = {},
+    disableLoadMenu: Boolean = false,
+    onClickLoad: () -> Unit = {},
+    onClickBack: () -> Unit = {},
+) {
+    var showMenu by remember { mutableStateOf(false) }
+
+    TopAppBar(
+        title = { Text("Cloud Marks Android") },
+        navigationIcon = if (showBackButton) {
+            {
+                IconButton(onClick = onClickBack) {
+                    Icon(Icons.Filled.ArrowBack, "Back")
+                }
+            }
+        } else {
+            {
+                Image(
+                    painterResource(R.drawable.ic_launcher),
+                    "Cloud Marks Android",
+                )
+            }
+        },
+        actions = {
+            IconButton(onClick = { showMenu = !showMenu }) {
+                Icon(Icons.Filled.Menu, "Menu")
+            }
+            DropdownMenu(
+                expanded = showMenu,
+                onDismissRequest = { showMenu = false }
+            ) {
+                DropdownMenuItem(
+                    onClick = {
+                        showMenu = false
+                        onClickSettings()
+                    },
+                    enabled = !disableSettingsMenu,
+                ) {
+                    Text("Settings")
+                }
+                DropdownMenuItem(
+                    onClick = {
+                        showMenu = false
+                        onClickLoad()
+                    },
+                    enabled = !disableLoadMenu,
+                ) {
+                    Text("Load")
+                }
+            }
+        }
+    )
+}
+
